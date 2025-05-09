52e090834994a7d984cf7e2453b193805b585abc
HoTienQuoc <hotienquoc0429@gmail.com>
2025-05-09 21:25:45 +0700

    new commit

--- src/main/java/com/k64cnttclc1/springbootblogapp/dto/CommentDto.java
+++ src/main/java/com/k64cnttclc1/springbootblogapp/dto/CommentDto.java
@@ -1,5 +1,6 @@
 package com.k64cnttclc1.springbootblogapp.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -11,8 +12,11 @@ import java.time.LocalDateTime;
 @Builder
 public class CommentDto {
     private Long id;
+    @NotEmpty
     private String name;
+    @NotEmpty(message = "Email not empty")
     private String email;
+    @NotEmpty(message = "Content not empty")
     private String content;
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
