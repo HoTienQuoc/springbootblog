3b57cc44343bd6decd29d73a646ac8f267d1e338
HoTienQuoc <hotienquoc0429@gmail.com>
2025-05-09 20:20:05 +0700

    new commit

--- src/main/java/com/k64cnttclc1/springbootblogapp/dto/CommentDto.java
+++ src/main/java/com/k64cnttclc1/springbootblogapp/dto/CommentDto.java
@@ -1,4 +1,19 @@
 package com.k64cnttclc1.springbootblogapp.dto;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CommentDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String content;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
 }
