3564de04076d13f98b8f8c1875a745c0ac8041a6
HoTienQuoc <hotienquoc0429@gmail.com>
2025-05-09 14:44:24 +0700

    new commit

--- src/main/java/com/k64cnttclc1/springbootblogapp/controller/BlogController.java
+++ src/main/java/com/k64cnttclc1/springbootblogapp/controller/BlogController.java
@@ -1,4 +1,25 @@
 package com.k64cnttclc1.springbootblogapp.controller;
 
+import com.k64cnttclc1.springbootblogapp.dto.PostDto;
+import com.k64cnttclc1.springbootblogapp.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class BlogController {
+    private PostService postService;
+
+    public BlogController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/")
+    public String viewBlogPosts(Model model) {
+        List<PostDto> postsResponse = postService.findAllPosts();
+        model.addAttribute("postsResponse", postsResponse);
+        return "view_posts";
+    }
 }
